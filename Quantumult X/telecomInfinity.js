9c731bbe9a27710135d569c73bf0abe9f23cc61b
Mr.Glasses <27768847+MisterGlasses@users.noreply.github.com>
2020-07-21 17:04:25 +0800

    Update telecomInfinity.js

--- Quantumult X/telecomInfinity.js
+++ Quantumult X/telecomInfinity.js
@@ -233,18 +233,18 @@ if(data.items[i].items[k].nameType == 331101){
  }
 }
     if (totalCommon) {
-        var flow = "【流量】已用:" + usagedCommon + "剩余:" + balanceCommon + "合计:" + totalCommon
+        var flow = "流量 用:" + usagedCommon + "余:" + balanceCommon + "共:" + totalCommon
         var message = flow
     }
     if (voiceAmount) {
-        var voice = "【通话】已用:" + voiceUsage + "分钟 剩余:" + voiceBalance + "分钟 合计:" + voiceAmount + "分钟"
+        var voice = "通话 用:" + voiceUsage + "分钟 余:" + voiceBalance + "分钟 共:" + voiceAmount + "分钟"
         message = message + "\n" + voice
     }
     if (msgAmount) {
-        var msginfo = "【短信】已用:" + msgUsage + "条 剩余:" + msgBalance + "条 合计:" + msgAmount + "条"
+        var msginfo = "短信 用:" + msgUsage + "条 余:" + msgBalance + "条 共:" + msgAmount + "条"
         message = message + "\n" + msginfo
     }
-	var fee = "【套餐】" + productname +  " 话费剩余:" + (balance / 100).toFixed(2) + "元"
+	var fee = "套餐 " + productname +  " 余额:" + (balance / 100).toFixed(2) + "元"
 	message = message + "\n" + fee
 
     if (bldata != '无'){message +=  `  ${m}月消费合计: `+ bldata.items[0].sumCharge/100+'元'}
